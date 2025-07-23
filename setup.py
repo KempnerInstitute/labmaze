5c44ece9e0ae4ec0038d14b91e66c7f26937b317
Grayson Martin <graysonmartin04@gmail.com>
2025-07-23 14:11:39 -0400

    Remove Bazel, use setuptools, replace Abseil functions

--- setup.py
+++ setup.py
@@ -15,121 +15,65 @@
 
 """Install script for setuptools."""
 
-import distutils.sysconfig
-import fnmatch
+from setuptools import setup, Extension, find_packages
+import pybind11
 import os
-import posixpath
-import re
-import shutil
-import sys
-import sysconfig
 
-import setuptools
-from setuptools.command import build_ext
+root = os.path.abspath(os.path.dirname(__file__))
+base_cc = os.path.join(root, "src", "labmaze", "cc")
 
-__version__ = '1.0.6'
-
-PROJECT_NAME = 'labmaze'
-
-REQUIRED_PACKAGES = [
-    'absl-py',
-    'numpy>=1.8.0',
-    'setuptools!=50.0.0',  # https://github.com/pypa/setuptools/issues/2350
+# Sources shared by both extensions
+core_srcs = [
+    os.path.join(base_cc, "algorithm.cc"),
+    os.path.join(base_cc, "char_grid.cc"),
+    os.path.join(base_cc, "flood_fill.cc"),
+    os.path.join(base_cc, "text_maze.cc"),
 ]
 
-WORKSPACE_PYTHON_HEADERS_PATTERN = re.compile(
-    r'(?<=path = ").*(?=",  # May be overwritten by setup\.py\.)')
+ext_modules = [
+    Extension(
+        name="labmaze.cc.python._random_maze",
+        sources=[
+            os.path.join(base_cc, "python", "_random_maze.cc"),
+            os.path.join(base_cc, "random_maze.cc"),
+            *core_srcs
+        ],
+        include_dirs=[
+            pybind11.get_include(),
+            base_cc,
+            os.path.join(base_cc, "python"),
+            root
+        ],
+        language="c++",
+        extra_compile_args=["/std:c++17"] if os.name == "nt" else ["-std=c++17"],
+    ),
+    Extension(
+        name="labmaze.cc.python._defaults",
+        sources=[
+            os.path.join(base_cc, "python", "_defaults.cc"),
+        ],
+        include_dirs=[
+            pybind11.get_include(),
+            base_cc,
+            os.path.join(base_cc, "python"),
+            root
+        ],
+        language="c++",
+        extra_compile_args=["/std:c++17"] if os.name == "nt" else ["-std=c++17"],
+    ),
+]
 
-IS_WINDOWS = sys.platform.startswith('win')
-
-
-class BazelExtension(setuptools.Extension):
-  """A C/C++ extension that is defined as a Bazel BUILD target."""
-
-  def __init__(self, bazel_target):
-    self.bazel_target = bazel_target
-    self.relpath, self.target_name = (
-        posixpath.relpath(bazel_target, '//').split(':'))
-    ext_name = os.path.join(
-        self.relpath.replace(posixpath.sep, os.path.sep), self.target_name)
-    setuptools.Extension.__init__(self, ext_name, sources=[])
-
-
-class BuildBazelExtension(build_ext.build_ext):
-  """A command that runs Bazel to build a C/C++ extension."""
-
-  def run(self):
-    for ext in self.extensions:
-      self.bazel_build(ext)
-    build_ext.build_ext.run(self)
-
-  def bazel_build(self, ext):
-    with open('WORKSPACE', 'r') as f:
-      workspace_contents = f.read()
-
-    with open('WORKSPACE', 'w') as f:
-      f.write(WORKSPACE_PYTHON_HEADERS_PATTERN.sub(
-          distutils.sysconfig.get_python_inc().replace(os.path.sep,
-                                                       posixpath.sep),
-          workspace_contents))
-
-    if not os.path.exists(self.build_temp):
-      os.makedirs(self.build_temp)
-
-    bazel_argv = [
-        'bazel',
-        'build',
-        ext.bazel_target,
-        '--symlink_prefix=' + os.path.join(self.build_temp, 'bazel-'),
-        '--compilation_mode=' + ('dbg' if self.debug else 'opt'),
-    ]
-
-    if IS_WINDOWS:
-      for library_dir in self.library_dirs:
-        bazel_argv.append('--linkopt=/LIBPATH:' + library_dir)
-      # TODO(stunya): Figure out why we need this.
-      if sysconfig.get_python_version() == '3.7':
-        bazel_argv.append('--linkopt=/LIBPATH:C:\\Python37\\Libs')
-
-    self.spawn(bazel_argv)
-
-    shared_lib_suffix = '.dll' if IS_WINDOWS else '.so'
-
-    ext_bazel_bin_path = os.path.join(
-        self.build_temp, 'bazel-bin',
-        ext.relpath, ext.target_name + shared_lib_suffix)
-    ext_dest_path = self.get_ext_fullpath(ext.name)
-    ext_dest_dir = os.path.dirname(ext_dest_path)
-    if not os.path.exists(ext_dest_dir):
-      os.makedirs(ext_dest_dir)
-    shutil.copyfile(ext_bazel_bin_path, ext_dest_path)
-
-
-def find_data_files(package_dir, patterns):
-  """Recursively finds files whose names match the given shell patterns."""
-  paths = set()
-  for directory, _, filenames in os.walk(package_dir):
-    for pattern in patterns:
-      for filename in fnmatch.filter(filenames, pattern):
-        # NB: paths must be relative to the package directory.
-        relative_dirpath = os.path.relpath(directory, package_dir)
-        paths.add(os.path.join(relative_dirpath, filename))
-  return list(paths)
-
-
-setuptools.setup(
-    name=PROJECT_NAME,
-    version=__version__,
-    description='LabMaze: DeepMind Lab\'s text maze generator.',
-    author='DeepMind',
-    license='Apache 2.0',
-    ext_modules=[
-        BazelExtension('//labmaze/cc/python:_defaults'),
-        BazelExtension('//labmaze/cc/python:_random_maze'),
-    ],
-    cmdclass=dict(build_ext=BuildBazelExtension),
-    packages=setuptools.find_packages(),
-    package_data={'labmaze':
-                  find_data_files(package_dir='labmaze', patterns=['*.png'])},
-    install_requires=REQUIRED_PACKAGES,
+setup(
+    name="labmaze",
+    version="1.0.6",
+    description="LabMaze: DeepMind Lab’s text maze generator",
+    author="DeepMind",
+    license="Apache-2.0",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    ext_modules=ext_modules,
+    install_requires=["numpy>=1.8.0", "absl-py"],
+    package_data={"labmaze": ["*.png"]},
+    zip_safe=False,
+    python_requires=">=3.7",
 )
